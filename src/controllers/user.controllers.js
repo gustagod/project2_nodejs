7bcd3e34f8c6e25e37704485c0746fc8106a75b8
gustagod <gustavoalain93@outlook.com>
2024-01-02 12:08:52 -0700

    first commit

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,51 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+
+const getAll = catchError(async(req, res) => {
+    const users = await User.findAll()
+    return res.json(users)
+});
+
+
+const create = catchError(async (req, res) => {
+    const { name, email, phone, birthday } = req.body
+    const user = await User.create({
+        name: name,
+        email: email,
+        phone: phone,
+        birthday: birthday
+    });
+    return res.status(201).json(user)
+})
+
+const getOne = catchError(async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    return res.json(user)
+})
+
+const remove = catchError(async (req, res) => {
+    const { id } = req.params;
+    await User.destroy({ where: { id: id } })
+    return res.sendStatus(204);
+})
+
+const update = catchError(async (req, res) => {
+    const { id } = req.params;
+    const { name, email, phone, birthday } = req.body;
+    const user = await User.update({
+        name: name,
+        email: email,
+        phone: phone,
+        birthday: birthday
+    }, { where: { id: id }, returning: true })
+    return res.json(user)
+})
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update
+}
